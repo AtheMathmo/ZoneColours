cdbdc70e04145127f7a81e0429c635537110b866
AtheMathmo <lucasj94@hotmail.co.uk>
2015-05-25 15:37:46 +0100

    Initial Commit

---
+++ ZoneColour/ZoneColourPanel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using ColossalFramework;
+using ColossalFramework.Plugins;
+using ColossalFramework.UI;
+using ColossalFramework.IO;
+using ColossalFramework.Math;
+using ColossalFramework.Steamworks;
+using ColossalFramework.Threading;
+using ICities;
+using UnityEngine;
+
+namespace ZoneColour
+{
+    class ZoneColourPanel : UIPanel
+    {
+        private UIPanel colorPanel;
+        private UIColorField colorField;
+
+        private int chosenColour = 0;
+
+        public UIPanel Parent { get; set; }
+
+        public int ChosenColour
+        {
+            get { return chosenColour; }
+            set { chosenColour = value; }
+        }
+
+        public override void Awake()
+        {
+            height = 20;
+            width = 20;
+        }
+
+        public override void Start()
+        {
+            colorField = Instantiate(FindObjectOfType<UIColorField>().gameObject).GetComponent<UIColorField>();
+            this.AttachUIComponent(colorField.gameObject);
+            colorField.name = "ZoneColorPicker";
+            colorField.size = new Vector2(20, 20);
+            colorField.relativePosition = new Vector3(0, 0);
+            colorField.pickerPosition = UIColorField.ColorPickerPosition.LeftBelow;
+            colorField.selectedColor = Singleton<ZoneManager>.instance.m_properties.m_zoneColors[ChosenColour];
+            colorField.eventSelectedColorChanged += (component, value) => Utils.SetZoneColour(value, ChosenColour);
+           
+            /*
+            textField.width = 40;
+            textField.relativePosition = new Vector3(0, 0);
+            textField.normalBgSprite = "TextFieldPanel";
+            textField.hoveredBgSprite = "TextFieldPanelHovered";
+            textField.focusedBgSprite = "TextFieldUnderline";
+
+            textField.text = "0";
+
+            textField.isInteractive = true;
+            textField.enabled = true;
+            textField.readOnly = false;
+            textField.builtinKeyNavigation = true; 
+             */
+        }
+
+        protected override void OnVisibilityChanged()
+        {
+            base.OnVisibilityChanged();
+
+        }
+
+        protected override void OnResolutionChanged(Vector2 previousResolution, Vector2 currentResolution)
+        {
+            base.OnResolutionChanged(previousResolution, currentResolution);
+        }
+
+    }
+}
